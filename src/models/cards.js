01f5d1d20e875263c746a49eedf624aec40ee594
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-06-21 09:50:22 -0300

    Craeted card model, card component and edited homepage

---
+++ src/models/cards.js
@@ -0,0 +1,11 @@
+const cartas = [
+    {value: 0, img: '../assets/apple.png', name: 'Apple'},
+    {value: 1, img: '../assets/bananas.png', name: 'Banana'},
+    {value: 2, img: '../assets/eggplant.png', name: 'Eggplant'},
+    {value: 3, img: '../assets/grape.png', name: 'Grape'},
+    {value: 4, img: '../assets/lemon.png', name: 'Lemon'},
+    {value: 5, img: '../assets/orange.png', name: 'Orange'},
+    {value: 6, img: '../assets/pear.png', name: 'Pear'},
+    {value: 7, img: '../assets/watermelon.png', name: 'Watermelon'},
+]
+export default cartas
