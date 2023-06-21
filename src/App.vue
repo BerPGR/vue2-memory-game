c5b45f92b82cc4e97453187ae591f778be200209
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-06-21 11:22:10 -0300

    little fix to image

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="app">
-    <Card :carta="cartas[1]"/>
+    <Card :carta="cartas[2]"/>
   </div>
 </template>
 
 <script>
 import Card from './components/Card.vue'
-import cards from './models/cards.js'
 
 export default {
   name: 'App',
@@ -14,13 +13,17 @@ export default {
     Card
   },
   data: () => ({
-    cartas: []
+    cartas: [
+      {value: 0, img: require('@/assets/apple.png'), name: 'Apple'},
+      {value: 1, img: require('@/assets/bananas.png'), name: 'Banana'},
+      {value: 2, img: require('@/assets/eggplant.png'), name: 'Eggplant'},
+      {value: 3, img: require('@/assets/grape.png'), name: 'Grape'},
+      {value: 4, img: require('@/assets/lemon.png'), name: 'Lemon'},
+      {value: 5, img: require('@/assets/orange.png'), name: 'Orange'},
+      {value: 6, img: require('@/assets/pear.png'), name: 'Pear'},
+      {value: 7, img: require('@/assets/watermelon.png'), name: 'Watermelon'},
+    ]
   }),
-  mounted() {
-    for (let carta of cards) {
-      this.cartas = [...this.cartas, carta]
-    }
-  },
 }
 </script>
 
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,26 +1,33 @@
 <template>
-  <div class="card-container" @click="showCartas" :style="{backgroundImage: 'url('+carta.img+')'}"></div>
+  <div class="card-container">
+    <img :src="carta.img" alt="">
+  </div>
 </template>
 
 <script>
-
 export default {
     name: 'CardComponent',
     props: {
         carta: Object
     },
-    methods: {
-        showCartas () {
-            console.log(this.carta.img);
-        }
-    }
 }
 </script>
 
-<style>
+<style scoped>
     .card-container {
         height: 200px;
-        width: 100px;
-        border: 1px solid black;
+        width: 125px;
+        border-radius: 25px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 2px solid black;
+        background-color: aqua;
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
     }
 </style>
